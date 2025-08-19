d227f2afa24e434c1ad2fea2f79a1ab78ae97aca
wahyudotdev <wahyudotdev@gmail.com>
2025-08-19 12:01:26 +0700

    fix: add config folder

--- internal/config/init.go
+++ internal/config/init.go
@@ -8,6 +8,7 @@ func init() {
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
 	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	viper.AddConfigPath("$HOME/.config/ez-snapshot")
 
 	// read config
 	if err := viper.ReadInConfig(); err != nil {
